fc51aab189b6921dd67ec16c2910df1b01b59ffd
swastij <60537632+swastij@users.noreply.github.com>
2020-06-06 20:05:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ class Post(db.Model):
     content=db.Column(db.Text, nullable=False)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #passing/referencing table name user and column id
     
-    def __repr__(self):     #magic fxn
+    def __repr__(self):    
         return f"Post ('{self.title}','{self.date_posted}') "
 
 post=[
