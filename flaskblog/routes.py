450eec5775f7ba5f7341934b2280d7324069ce64
Swasti Jain <swastijain04@gmail.com>
2020-06-12 17:12:26 +0530

    pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required #c
 @app.route('/')        
 @app.route('/home')
 def home():
-    posts= Post.query.all()
+    page= request.args.get('page', 1, type=int) #default value of page is 1 , getting page no from url
+    posts= Post.query.order_by(Post.date_posted.desc()).paginate(page=page ,per_page=2) #to show by latest post; 2 posts per page
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
@@ -133,3 +134,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted', 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>") #this route is for redirecting to user posts when clicked on username area of the posts
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=2)
+    return render_template('user_posts.html', posts=posts, user=user)
